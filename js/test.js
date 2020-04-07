091c689bb32a1f56a8d269884b3ad572ff8de486
Beldim <dimonja@inbox.ru>
2020-04-07 14:01:11 +0300

    Tasks on the basics of working with DOM in JavaScript

--- js/test.js
+++ js/test.js
@@ -1,1 +1,50 @@
-﻿
+﻿/* Задачи на основы работы с DOM в JavaScript */
+/* 6 */
+/* function buttonClick(){
+   let input = document.getElementById("input");
+   alert(input.value);
+} */
+/* 7 */
+/* function buttonClick(){
+    let img = document.getElementById("img");
+    img.src = src="img/2.jpg";
+} */
+/* 8 */
+/* function buttonClick(elem){
+    alert(elem.value);
+} */
+/* 9 */
+/* function buttonClick(elem){
+    alert(elem.value);
+    elem.value="Теперь я поменяла текст";
+} */
+/* 10 */
+/* function on(elem1){
+    elem1.value = "Теперь я поменяла текст";
+}
+
+function stop(elem2){
+    elem2.value = "Ой, еще раз поменяла текст";
+} */
+/* 11 */
+/* function rename(name){
+    name.value = 'Я поменяла название'
+} */
+/* 12 */
+/* function stop(elem){
+    elem.value = "Теперь я нажата и не активна";
+    elem.disabled = "true";
+} */
+/* 13 */
+/* function over(imaging){
+    imaging.src = "img/2.jpg";
+}
+function out(imaging){
+    imaging.src = "img/1.jpg";
+    } */
+/* 14 */
+function go(){
+   let inp = document.getElementById('input');
+   inp.style.color = 'red';
+   inp.style.width = '400px';
+}
